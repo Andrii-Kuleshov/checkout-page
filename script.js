12361683c4d5b60507354fb293d099ff38b55f6d
Andrii <andreiandreik19@gmail.com>
2026-03-13 19:24:45 +0200

    Update script.js

--- script.js
+++ script.js
@@ -11,3 +11,21 @@ value = value.slice(0,2) + "/" + value.slice(2,4);
 e.target.value = value;
 
 });
+
+const cardNumber = document.getElementById("card-number");
+
+cardNumber.addEventListener("input", function(e) {
+
+let value = e.target.value.replace(/\D/g, "");
+
+value = value.substring(0,16);
+
+let formatted = value.match(/.{1,4}/g);
+
+if(formatted){
+e.target.value = formatted.join(" ");
+} else {
+e.target.value = value;
+}
+
+});
